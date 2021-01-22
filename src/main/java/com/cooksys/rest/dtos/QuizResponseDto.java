79116a2371ffb586345b0c281661f71c8beedaf2
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-01-21 21:22:56 -0500

    Quiz Response generated

---
+++ src/main/java/com/cooksys/rest/dtos/QuizResponseDto.java
@@ -0,0 +1,16 @@
+package com.cooksys.rest.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class QuizResponseDto {
+    private Long id;
+    private String name;
+    private List<ItemResponseDto> questions;
+}
