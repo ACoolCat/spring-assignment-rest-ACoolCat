3c9b69db03fd006170b096e48a7797009d82d297
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-01-21 21:00:20 -0500

    Question entity generated

---
+++ src/main/java/com/cooksys/rest/entities/Question.java
@@ -0,0 +1,27 @@
+package com.cooksys.rest.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Question {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String questionText;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private List<Answer> answers;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Quiz quiz;
+}
