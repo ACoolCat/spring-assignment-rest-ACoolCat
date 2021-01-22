70b63eb3972c5a9b0fa7a013466e0fb1bb4259c2
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-01-21 22:17:20 -0500

    quiz service

---
+++ src/main/java/com/cooksys/rest/services/QuizService.java
@@ -0,0 +1,35 @@
+package com.cooksys.rest.services;
+
+import com.cooksys.rest.dtos.QuizRequestDto;
+import com.cooksys.rest.dtos.QuizResponseDto;
+import com.cooksys.rest.entities.Question;
+import com.cooksys.rest.entities.Quiz;
+import com.cooksys.rest.mappers.QuizMapper;
+import com.cooksys.rest.repository.QuizRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class QuizService {
+
+    private QuizRepository quizRepository;
+    private QuizMapper quizMapper;
+
+    public ResponseEntity<QuizResponseDto> createQuiz(QuizRequestDto quizRequestDto) {
+        Quiz quiz = quizMapper.requestDtoToEntity(quizRequestDto);
+
+        for(Question question : quiz.getQuestion()) {
+            question.setQuiz(quiz);
+        }
+
+        for(Answer answer : question.getAnswer()){
+            answer.setQuestion(question)
+        }
+
+        Quiz savedQuiz = quizRepository.saveAndFlush(quiz);
+        return new ResponseEntity<>(quizMapper.entityToResponseDto(savedQuiz), HttpStatus.CREATED);
+    }
+}
