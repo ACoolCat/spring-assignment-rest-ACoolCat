a1121017ea50671f98917732574bc2e11e78ce93
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-01-21 21:53:20 -0500

    answer request added

--- src/main/java/com/cooksys/rest/dtos/AnswerRequestDto.java
+++ src/main/java/com/cooksys/rest/dtos/AnswerRequestDto.java
@@ -1,4 +1,15 @@
 package com.cooksys.rest.dtos;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AnswerRequestDto {
+    private String answerText;
+    private Boolean isCorrect;
 }
